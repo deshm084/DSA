7f8c65a43a68e92987a06ef06fe64011d9dd75b1
SANSKRUTI SANJAY DESHMUKH <deshm084@umn.edu>
2026-01-10 13:49:32 -0600

    Time: 3 ms (88.32%) | Memory: 20.5 MB (5.81%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def findCircleNum(self, M):
+        seen = set([])
+        res = 0
+        for i in range(len(M)):
+            if i not in seen:
+                toSee = [i]
+                while len(toSee):
+                    cur = toSee.pop()
+                    if cur not in seen:
+                        seen.add(cur)
+                        toSee = [j for j,v in enumerate(M[cur]) if v and j not in seen] + toSee
+                res += 1
+        return res
+        
