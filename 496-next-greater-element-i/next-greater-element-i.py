ff1a1d66e4e373b11a61096ce1f7971b4ac6ad7c
SANSKRUTI SANJAY DESHMUKH <deshm084@umn.edu>
2026-01-17 23:07:45 -0600

    Time: 0 ms (100.00%) | Memory: 19.6 MB (7.27%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -0,0 +1,15 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        stack = []
+        next_greater = {}
+        for num in nums2:
+            while stack and num > stack[-1]:
+                smaller = stack.pop()
+                next_greater[smaller] = num
+            stack.append(num)
+        while stack:
+            next_greater[stack.pop()] = -1
+        result = []
+        for num in nums1:
+            result.append(next_greater[num])
+        return result
