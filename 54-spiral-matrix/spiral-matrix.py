2e2c110e64819986f7cf92daabd3935385706c9f
SANSKRUTI SANJAY DESHMUKH <deshm084@umn.edu>
2026-01-24 12:04:10 -0600

    Time: 0 ms (100.00%) | Memory: 19.5 MB (12.32%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -1,10 +1,10 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        if not matrix:
+        if not matrix or not matrix[0]:
             return []
         result = []
         top = 0
-        bottom =len(matrix) - 1
+        bottom = len(matrix) - 1
         left = 0
         right = len(matrix[0]) - 1
         while top <= bottom and left <= right:
@@ -19,14 +19,8 @@ class Solution:
                     result.append(matrix[bottom][col])
                 bottom -= 1
             if left <= right:
-                for row in range(bottom, top - 1, -1):
+                for row in range(bottom, top -1, -1):
                     result.append(matrix[row][left])
                 left += 1
-        return result
+        return result 
 
-
-
-
-
-
-        
