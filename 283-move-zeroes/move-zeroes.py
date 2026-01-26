20b7e49f11d012b4320bbc3b9e59809cc87baa2b
SANSKRUTI SANJAY DESHMUKH <deshm084@umn.edu>
2026-01-26 08:28:56 -0600

    Time: 4 ms (60.19%) | Memory: 20.4 MB (34.07%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -3,10 +3,11 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        insert_pos = 0
-
+        pos = 0
         for i in range(len(nums)):
             if nums[i] != 0:
-                nums[i], nums[insert_pos] = nums[insert_pos], nums[i]
-
-                insert_pos += 1
+                nums[pos] = nums[i]
+                pos += 1
+        for i in range(pos, len(nums)):
+            nums[i] = 0
+        
